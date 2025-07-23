b553072bd688fec454a081702b9f14329660d16c
nepstech <majhintech@gmail.com>
2025-07-23 22:29:10 +0300

    add 01-basic veriables.

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,7 @@
+const userId = "SU0121240";
+let username = "nephx";
+let city = "kathmandu";
+let phNumber = "123456456";
+let email = "email@mail.com";
+
+console.table([userId, username, city, phNumber, email]);
