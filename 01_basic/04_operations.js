1cc9c364fa3648e32ff15a2af0bf6302c57b3a6c
nepstech <majhintech@gmail.com>
2025-07-24 22:39:04 +0300

    add operations

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,20 @@
+// operations
+let value = 3;
+
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+
+let str = "nephx";
+let str1 = "majhi";
+
+addStr = str + str1;
+// console.log(addStr);
+
+console.log(true);
+console.log(+"");
+
+let gateCounter = 100;
+++gateCounter;
+console.log(gateCounter);
