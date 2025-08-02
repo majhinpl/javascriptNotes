4aa4eb6f926e054f7c0638494e6c09f6e78c0673
nepstech <majhintech@gmail.com>
2025-08-02 23:00:34 +0300

    add do while loops.

---
+++ 06_loops/02_loopsWlile.js
@@ -0,0 +1,22 @@
+let index = 0;
+
+while (index <= 10) {
+  console.log(`Value of index is : ${index}`);
+  index = index + 2;
+}
+
+let stocks = ["lec", "kkhc", "nhdl", "hdiclp"];
+
+let arr = 0;
+
+while (arr < stocks.length) {
+  console.log(`Value is : ${stocks[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 1;
+
+do {
+  console.log(`score is ${score}`);
+  score++;
+} while (score <= 10);
