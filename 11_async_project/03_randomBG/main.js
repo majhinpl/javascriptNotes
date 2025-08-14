287edc15f0dc1e37ab47affb987d341de65407d7
Nepal Majhi <majhinpl@gmail.com>
2025-08-14 07:23:29 +0300

    add project keyboard

--- 11_async_project/03_randomBG/main.js
+++ 11_async_project/03_randomBG/main.js
@@ -1,31 +1,19 @@
-// Generate a random color
-
-const randomColor = function () {
-  const hex = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += hex[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-let intervalId;
-
-const startChangingColor = () => {
-  if (!intervalId) {
-    intervalId = setInterval(changeBGColor, 2000);
-  }
-
-  function changeBGColor() {
-    document.body.style.backgroundColor = randomColor();
-  }
-};
-
-const stopChangingColor = () => {
-  clearInterval(intervalId);
-  intervalId = null;
-};
-
-document.querySelector("#start").addEventListener("click", startChangingColor);
-
-document.querySelector("#stop").addEventListener("click", stopChangingColor);
+const insert = document.getElementById("insert");
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+  <div class="color">
+    <table>
+      <tr>
+        <th>Key</th>
+        <th>Keycode</th>
+        <th>Code</th>
+      </tr>
+      <tr>
+        <td>${e.key === " " ? "Space" : e.key}</td>
+        <td>${e.keyCode}</td>
+        <td>${e.code}</td>
+      </tr>
+    </table>  
+  </div>
+  `;
+});
