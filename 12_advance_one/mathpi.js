0acc857afba1c0acc823b6ef0da2932ffbffb016
nepstech <majhintech@gmail.com>
2025-08-16 22:02:46 +0300

    add Getter Setter in js

---
+++ 12_advance_one/mathpi.js
@@ -0,0 +1,26 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
+
+console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+const chai = {
+  name: "genger chai",
+  price: 250,
+  isAvailable: true,
+};
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+  writable: false,
+  enumerable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+  console.log(`${key} : ${value}`);
+}
